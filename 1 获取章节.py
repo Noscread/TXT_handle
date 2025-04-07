f82e17c2c0bedfeb5ddbcac58d698fd0fa369c78
Dfxc <33346305+Noscread@users.noreply.github.com>
2025-04-07 16:16:48 +0800

    获取章节

--- 1 获取章节.py
+++ 1 获取章节.py
@@ -17,7 +17,7 @@ def read_txt(file_path):
                 # 正则表达式，匹配目录
                 pattern = (r'(?:'
                            r'第[\s一二三四五六七八九十百千0-9]+(?:卷|章|节)' # \s 代表空格 ?: 代表非捕获组
-                           # r'|[0-9]+\.' # 01.
+                           r'|[0-9]+(?:\.|\．)' # 01.
                            r'|\([0-9]+\)'
                            r')')
                 matches = re.findall(pattern, line)
@@ -62,10 +62,10 @@ def read_txt(file_path):
 if __name__ == "__main__":
     file_path = '测试.txt'
     contents = read_txt(file_path)
-    # print(contents.get('小说目录', '默认值'))
+    print(contents.get('小说目录', '默认值'))
     # print(contents.get('小说正文', '默认值'))
 
     小说目录 = contents.get('小说目录', '')
     小说正文 = contents.get('小说正文', '')
-    print(小说目录[1])
-    print(小说正文[1])
+    # print(小说目录[1])
+    # print(小说正文[1])
